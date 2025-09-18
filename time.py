ca3c1da5ffd3b52d69285a9f0e3aa95bf6c2df61
Md Zahid Ansari <124868102+zahidans@users.noreply.github.com>
2025-09-18 13:19:48 -0300

    Add files via upload

---
+++ time.py
@@ -0,0 +1,18 @@
+# this code is time consuming 
+import time
+
+timestamp = time.strftime("%H:%M:%S")
+print(timestamp)
+
+timestamp = time.strftime("Day: %d - Month: %m - Year: %Y")
+print(timestamp)
+
+timestamp = time.strftime("hours: %H")
+print(timestamp)
+
+timestamp = time.strftime("minute: %M")
+print(timestamp)
+
+timestamp = time.strftime("second: %S")
+print(timestamp) 
+
